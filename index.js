c0f92f2d6c273cbd5eae07caa223f9635a8d62fd
Maxis Kao <maxis1718@gmail.com>
2016-07-13 19:47:55 +0800

    connect facebook

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ var port = process.env.PORT || 3000;
 var router = express.Router();
 var helmet = require('helmet');
 var cors = require('cors');
+var _get = require('lodash/get');
+var likes = [
+    '851582_369239386556143_1497813874',
+    '851587_369239346556147_162929011',
+    '851557_369239266556155_759568595'
+];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -24,12 +30,38 @@ router.get('/', function(req, res) {
 */
 router.post('/index', function(req, res, next) {
     var body = req.body;
+    var attachment;
+    var message = body.message || '';
+    var messageType = body.type || 'text';
+    var userid = body.userid || 'john_doe';
 
-    // get user text
-    var text = body.text;
+    var messageText = '';
 
-    console.log('>>> received:', body);
-    res.status(200).json({status: 'got', msg: 'kerker'});
+    // process received message
+    if(messageType === 'attachments') {
+        // attachment = message && message.length && message[0];
+        var sticker_url = _get(message, '0.payload.url', '');
+        for (var i=0; i<likes.length; i++) {
+            if(sticker_url.indexOf(likes[i]) > 0) {
+                messageText = '[LIKE]';
+            }
+        }
+    }
+    else if (messageType === 'text') {
+        // pick a message from message pool
+        // TODO
+        if (message == '收到') {
+            //
+        } else {
+            messageText = message;
+        }
+    }
+
+    if (messageText) {
+        console.log('messageText:', messageText);
+    }
+
+    res.status(200).json({text: 'kerker'});
 });
 
 /*
