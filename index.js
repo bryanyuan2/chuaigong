3cce412b115a9e5ee1d520109928675c304dd49c
Maxis Kao <maxis1718@gmail.com>
2016-07-13 21:00:14 +0800

    add rule

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ var likes = [
     '851557_369239266556155_759568595'
 ];
 
+var msgs = ['大聲講話啊', '一起踹共啊', '快點打字啊'];
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(helmet());
@@ -60,8 +62,8 @@ router.post('/index', function(req, res, next) {
     else if (messageType === 'text') {
         // pick a message from message pool
         // TODO
-        if (message == '收到') {
-            //
+        if (msgs.indexOf(message) >= 0) {
+            // do nothing
         } else {
             messageText = message;
         }
