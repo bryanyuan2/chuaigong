50a9aba378ac0dd0528d38ed587e2ee1283996ce
Bryan Cheng <cycheng@yahoo-inc.com>
2016-07-13 14:21:06 +0800

    added action

--- routes/video.js
+++ routes/video.js
@@ -8,25 +8,38 @@ botModel.getVideo = function(req, res) {
         CONST_LOG_FILENAME = "comment",
         CONST_TIME_INTERVAL = 3000,
         CONST_VIDEO_HTML_SOURCE = "video.html";
-
-    /* read comment */
-    fs.readFile( path.join(__dirname+'/'+ CONST_LOG_FILENAME), 'utf8', function (err,data) {
-      if (err) {
-        return console.log(err);
-      }
-
-      data = data.split('\n');
-      for(var i=0;i<data.length-1;i++) {
-        var line = data[i].split('|');
-        var target_comment = line[0],
-            target_timestamp = line[1];
-        console.log(target_comment, target_timestamp);
-      }
-    });
+    var previous_timestamp = 0,
+        current_timestamp = 0;
 
     /* setInterval and reach comment */
     setInterval(function(){
-      console.log("Hello");
+
+      current_timestamp = new Date().getTime();
+
+      console.log("current_timestamp = " + current_timestamp);
+      console.log("previous_timestamp = " + previous_timestamp);
+
+      /* read comment */
+      fs.readFile( path.join(__dirname+'/'+ CONST_LOG_FILENAME), 'utf8', function (err,data) {
+        if (err) {
+          return console.log(err);
+        }
+
+        data = data.split('\n');
+        for(var i=0;i<data.length-1;i++) {
+          var line = data[i].split('|');
+          var target_comment = line[0],
+              target_timestamp = line[1];
+
+          if (target_timestamp > previous_timestamp && target_timestamp <= current_timestamp) {
+            // show up comment
+            console.log(target_comment);
+          } else {
+            console.log("no latest comment");
+          }
+        }
+        previous_timestamp = current_timestamp;
+      });
     }, CONST_TIME_INTERVAL);
 
 
